b090a3d9cfed79a381762a9bda1aa972461c94c8
Saurav  Kanchan <sauravnk30@gmail.com>
2022-02-24 11:15:00 +0530

    Update custom.js

--- custom.js
+++ custom.js
@@ -2,6 +2,6 @@
   wallet = await new ethers.Wallet.createRandom();
   console.log(`Loaded wallet ${wallet.address}`);
   privateKey = wallet.privateKey;
-  publicKey = wallet.signingKey.publicKey;
+  publicKey = wallet.publicKey;
   web3 = web3 = new Web3(window.ethereum);
 })();
